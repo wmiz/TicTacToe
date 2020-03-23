93e44eb47fe6df96db6e6285bca54285db303e9c
wmisback <will.misback@gmail.com>
2020-03-23 13:23:27 -0400

    Add displayBoard function and setup example board

--- scripts.js
+++ scripts.js
@@ -1,1 +1,19 @@
-// 
+let GameBoard = (() => {
+
+	let board = ['X', ' ', 'O', 'X', ' ', 'O', 'X', '', 'O'];
+	function displayBoard() {
+		let boardElement = document.querySelector("#board");
+		let entryElements = boardElement.querySelectorAll("p");
+		console.log(entryElements);
+		let i = 0;
+		entryElements.forEach(element => {
+			element.innerHTML = board[i++]; 
+		})
+	}
+
+	return {
+		displayBoard: displayBoard
+	}
+})();
+
+GameBoard.displayBoard();
